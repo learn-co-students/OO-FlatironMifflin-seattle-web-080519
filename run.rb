bd708f8d1022c3a2ae772b5297ff3e88a136954d
github username <github email address>
2019-08-09 18:38:24 -0700

    added more seeds in run.rb

--- run.rb
+++ run.rb
@@ -10,5 +10,13 @@ ryan  = Manager.new("Ryan", "Sales", 35)
 joe = Manager.new("Joe", "Legal", 49)
 rebecca = Manager.new("Rebecca", "HS&E", 37)
 
+arnie = Employee.new("Arnie", 30000, sarah)
+chris = Employee.new("Chris", 40000, ryan)
+alex = Employee.new("Alex", 35000, sarah)
+bob = Employee.new("Bob", 70000, joe)
+alice = Employee.new("Alice", 92500, sarah)
+
+
+
 binding.pry
 puts "done"
